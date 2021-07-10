4607e919df4b2be4392571ed6fc0fea925c12829
Jung Nam <j.nam0808@gmail.com>
2021-07-10 00:31:46 -0400

    added server.js

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-	useNewUrlParser: true,
-	useFindAndModify: false,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-	console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
